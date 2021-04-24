fa627f884f81cb0c43e5136b8a9ed226c8a09214
lalit <lalitrajput069@gmail.com>
2021-04-24 13:12:11 +0530

    Added ViewModel functionality

--- app/src/main/java/com/example/livedatapractise/LiveDataPractiseViewModel.kt
+++ app/src/main/java/com/example/livedatapractise/LiveDataPractiseViewModel.kt
@@ -3,5 +3,10 @@ package com.example.livedatapractise
 import androidx.lifecycle.ViewModel
 
 class LiveDataPractiseViewModel : ViewModel() {
+    var currentNumber:Int = 0
+
+    fun numberIncrement(){
+        currentNumber++
+    }
 
 }
--- app/src/main/java/com/example/livedatapractise/MainActivity.kt
+++ app/src/main/java/com/example/livedatapractise/MainActivity.kt
@@ -3,18 +3,21 @@ package com.example.livedatapractise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.livedatapractise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
-    private var number:Int = 1
+    private lateinit var viewModel: LiveDataPractiseViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(LiveDataPractiseViewModel::class.java)
         binding.apply {
+            displayNumber.text = viewModel.currentNumber.toString()
             button.setOnClickListener {
-                displayNumber.text = number.toString()
-                number++
+                viewModel.numberIncrement()
+                    displayNumber.text = viewModel.currentNumber.toString()
             }
         }
     }
