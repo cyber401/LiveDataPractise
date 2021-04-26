5ffa2bc265a38a02005163aaca12eb56a2ad6af2
lalit <lalitrajput069@gmail.com>
2021-04-26 13:13:45 +0530

    Just added LiveData in activity_main.xml file rather then adding LiveData observer in MainActivity.kt and it help to reduce boiler plate code in the class

--- app/src/main/java/com/example/livedatapractise/MainActivity.kt
+++ app/src/main/java/com/example/livedatapractise/MainActivity.kt
@@ -10,15 +10,13 @@ import androidx.lifecycle.liveData
 import com.example.livedatapractise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: LiveDataPractiseViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
         viewModel = ViewModelProvider(this).get(LiveDataPractiseViewModel::class.java)
         binding.masterViewModel = viewModel
-        viewModel.value().observe(this, Observer {
-            binding.displayNumber.text = it.toString()
-        })
     }
 }
